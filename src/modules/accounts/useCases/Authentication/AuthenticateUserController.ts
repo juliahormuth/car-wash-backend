dadac713de5edf65a202506266710d194262bff9
Julia Hormuth <jvhormuth@gmail.com>
2021-10-17 20:52:23 -0300

    added the authentication with token

--- src/modules/accounts/useCases/Authentication/AuthenticateUserController.ts
+++ src/modules/accounts/useCases/Authentication/AuthenticateUserController.ts
@@ -1,20 +1,21 @@
-import { Request, response, Response } from 'express';
+import { Request, Response } from 'express';
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
 import { container } from 'tsyringe';
 
 class AuthenticateUserController{
 
-    async handle(request: Request, response: Response ){
+    async handle(request: Request, response: Response ): Promise<Response>{
     
-        const { password, email } = request.body;
+        const { email, password } = request.body;
     
      const authenticateUserUseCase = container.resolve(AuthenticateUserUseCase)
 
-     const authenticateInfo = await authenticateUserUseCase.execute({email, password})
+     const token = await authenticateUserUseCase.execute({
+       email, 
+       password,
+      });
 
-     return response.json(authenticateInfo)
-
-
+     return response.json(token)
   }
 }
 export { AuthenticateUserController };
--- src/modules/accounts/useCases/Authentication/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/Authentication/AuthenticateUserUseCase.ts
@@ -44,16 +44,16 @@ class AuthenticateUserUseCase {
           expiresIn: "1d"
       });
         
-      return {
-          user,
-          token,
-      }
-          
+     const tokenReturn: IResponse =  {
+         token,
+         user: {
+             name: user.name,
+             email: user.email,
+         }
+        };
+          return tokenReturn
     }
-
-
 }
 
- 
 
 export { AuthenticateUserUseCase }
