d0d5f5fe4344800097747e068d00fc054ac2891d
Julia Hormuth <jvhormuth@gmail.com>
2021-10-18 21:37:28 -0300

    added the types user and update avatar controller

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -32,6 +32,11 @@ export async function ensureAuthenticated(
   if(!user){
       throw new AppError("User does not exists!", 401)
   }
+
+  request.user = {
+    id: user_id,
+  }
+
    next();
     } catch {
         throw new AppError ( "Invalid token!", 401);
