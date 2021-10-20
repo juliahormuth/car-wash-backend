dee0a681afd279ab5a1571b32cfb007dda371bb6
Julia Hormuth <jvhormuth@gmail.com>
2021-10-20 00:20:31 -0300

    create spec test

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,11 @@
+
+
+describe("Criar categorya", () => {
+    
+    it("Espero que 2 + 2 seja 4", ()  => {
+        const soma = 2 + 2
+        const resultado = 4
+
+        expect(soma).toBe(resultado)
+    })
+})
