c67000950e12437ff60159731ab3f14e3c413d69
Julia Hormuth <jvhormuth@gmail.com>
2021-10-19 22:04:51 -0300

    reload picture profile

--- src/modules/accounts/useCases/UpdateUserVatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/UpdateUserVatar/UpdateUserAvatarUseCase.ts
@@ -1,9 +1,6 @@
-//Configuração upload mutter
-//Criar regra de negócio do upload
-//Criar controller
-
 import { inject, injectable } from "tsyringe";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { deleteFile } from '../../../../utils/file';
 
 interface IRequest {
   user_id: string;
@@ -22,6 +19,10 @@ class UpdateUserAvatarUseCase {
     async execute({user_id, avatar_file}: IRequest): Promise<void>{
       const user = await this.usersRepository.findById(user_id);
 
+      if(user.avatar){
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+      }
+      
       user.avatar = avatar_file;
 
       await this.usersRepository.create(user);
