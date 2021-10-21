cc15be6443346893bc5fdce6e8da45e306b760a4
Julia Hormuth <jvhormuth@gmail.com>
2021-10-20 21:54:29 -0300

    added first unit test

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,11 +1,51 @@
+import { AppError } from "../../../../errors/AppError";
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoryRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase"
 
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
 
-describe("Criar categorya", () => {
-    
-    it("Espero que 2 + 2 seja 4", ()  => {
-        const soma = 2 + 2
-        const resultado = 4
+describe("Create Category", () => {
+    beforeEach( () => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory
+    );
 
-        expect(soma).toBe(resultado)
-    })
-})
+});
+
+    it("should be able to create a new category", async () => {
+        const category = {
+            name: "Category Test",
+            description: "Category description test",
+        };
+       await createCategoryUseCase.execute({
+            name: category.name,
+            description: category.description,
+
+        });
+        const categoryCreated = await categoriesRepositoryInMemory.findByName(category.name)
+
+        expect(categoryCreated).toHaveProperty("id");
+    });
+
+    it("should not be able to create a new category with name exists", async () => {
+      expect(async () => {
+      
+        const category = {
+            name: "Category Test",
+            description: "Category description test",
+        };
+       await createCategoryUseCase.execute({
+            name: category.name,
+            description: category.description,
+        });
+
+        await createCategoryUseCase.execute({
+            name: category.name,
+            description: category.description,
+
+        });        
+
+    }).rejects.toBeInstanceOf(AppError)
+      })
+});
