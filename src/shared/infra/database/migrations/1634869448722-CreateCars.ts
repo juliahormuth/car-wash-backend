a3771354b503541cf789d647fe71006b8b4cddea
Julia Hormuth <jvhormuth@gmail.com>
2021-10-24 11:38:58 -0300

    fix migration

--- src/shared/infra/database/migrations/1634869448722-CreateCars.ts
+++ src/shared/infra/database/migrations/1634869448722-CreateCars.ts
@@ -6,65 +6,64 @@ export class CreateCars1634869448722 implements MigrationInterface {
         await queryRunner.createTable(
             new Table({
                 name: "cars",
-                columns: [
-                    {
-                        name: "id",
-                        type: "uuid",
-                        isPrimary: true,
-                    },
-                    {
-                        name: "name",
-                        type: "varchar",
-                    },
-                    {
-                        name: "description",
-                        type: "varchar",
-                    },
-                    {
-                         name: "daily_rate",
-                         type: "numeric",
-                    },
-                    {
-                        name: "available",
-                        type:  "boolean",
-                        default: true,
-                    },
-                    {
-                        name: "license_plate",
-                        type: "varchar",
-                    },
-                    {
-                        name: "fine_amount",
-                        type: "numeric",
-                    },
-                    {
-                        name: "brand",
-                        type: "varchar",
-                    },
-                    {
-                        name: "category_id",
-                        type: "uuid",
-                        isNullable: true
-                    },
-                    {
-                        name: "created_at",
-                        type: "timestamp",
-                        default: "now(),"
-                    }
-                
-                ],
-                foreignKeys: [
-                    {
-                        name: "FKCategoryCar",
-                        referencedTableName: "categories",
-                        referencedColumnNames: ["id"],
-                        columnNames: ["category_id"],
-                        onDelete: "SET_NULL",
-                        onUpdate: "SET_NULL"
-                    }
-                ]
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "daily_rate",
+            type: "numeric",
+          },
+          {
+            name: "available",
+            type: "boolean",
+            default: true,
+          },
+          {
+            name: "license_plate",
+            type: "varchar",
+          },
+          {
+            name: "fine_amount",
+            type: "numeric",
+          },
+          {
+            name: "brand",
+            type: "varchar",
+          },
+          {
+            name: "category_id",
+            type: "uuid",
+            isNullable: true,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FK_Category_Car",
+            referencedTableName: "categories",
+            referencedColumnNames: ["id"],
+            columnNames: ["category_id"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+        ],
             })
-        )
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
